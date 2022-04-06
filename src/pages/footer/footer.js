3911d6450de2563c5ab4b1de068b87cbd4777fc3
Arun Govindappa <govindappaarun@gmail.com>
2022-04-06 05:51:29 +0530

    update routes and added footer

--- src/pages/footer/footer.js
+++ src/pages/footer/footer.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Box } from "src/components";
-import { Wrapper } from "./footer.styled";
+import { StyledAnchor, Wrapper } from "./footer.styled";
+import { BsTwitter, BsGithub } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <Wrapper>
       <Box>All rights reserved</Box>
+      <Box className="ml-auto">
+        <StyledAnchor href="https://github.com/govindappaarun">
+          <BsTwitter />
+        </StyledAnchor>
+        <StyledAnchor href="https://twitter.com/govindappaarun">
+          <BsGithub />
+        </StyledAnchor>
+      </Box>
     </Wrapper>
   );
 }
--- src/pages/footer/footer.styled.js
+++ src/pages/footer/footer.styled.js
@@ -4,4 +4,11 @@ export const Wrapper = styled.div`
   background-color: ${(p) => p.theme.neutral.dark};
   padding: 1rem;
   color: ${(p) => p.theme.primary.contrastText};
+  display: flex;
+`;
+
+export const StyledAnchor = styled.a`
+  color: ${(p) => p.theme.white};
+  margin: 0 0.5rem;
+  font-size: 1.2rem;
 `;
