e01bfc66ed3ed0430c29a256b6b1960bc46ecf35
Arun Govindappa <govindappaarun@gmail.com>
2022-06-17 22:55:34 +0530

    fix footer

--- src/pages/footer/footer.js
+++ src/pages/footer/footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Box } from "src/components";
+import { Box, Typography } from "src/components";
 import { StyledAnchor, Wrapper } from "./footer.styled";
 import { BsTwitter, BsGithub } from "react-icons/bs";
 
 export default function Footer() {
   return (
     <Wrapper>
-      <Box>All rights reserved</Box>
+      <Typography variant="h2">All rights reserved</Typography>
       <Box className="ml-auto" display="flex" gap="md">
         <StyledAnchor href="https://twitter.com/govindappaarun">
           <BsTwitter />
